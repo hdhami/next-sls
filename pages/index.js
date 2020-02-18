cbc08e24f55a26e979d2bb6722354f04582c929a
hdhami <hsd.1989@gmail.com>
2020-02-18 12:50:57 +0530

    added pages

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+    return (
+        <div>
+            <p>Welcome to Next</p>
+        </div>
+    );
+};
+
+export default Index;
---
+++ pages/login.js
@@ -0,0 +1,7 @@
+const Login = () => (
+    <div>
+        <p>Welcome to Login Page</p>
+    </div>
+);
+
+export default Login;
